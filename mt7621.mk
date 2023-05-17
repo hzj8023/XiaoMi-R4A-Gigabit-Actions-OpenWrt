b78c1f79a5e1dec6cae4402f2c707fe621706aee
hzj8023 <91045189+hzj8023@users.noreply.github.com>
2023-05-17 10:27:43 +0800

    Update mt7621.mk

--- mt7621.mk
+++ mt7621.mk
@@ -1744,7 +1744,7 @@ TARGET_DEVICES += xiaomi_mi-router-4
 define Device/xiaomi_mi-router-4a-gigabit
   $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
-  IMAGE_SIZE := 16064k
+  IMAGE_SIZE := 32064k
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := Mi Router 4A
   DEVICE_VARIANT := Gigabit Edition
